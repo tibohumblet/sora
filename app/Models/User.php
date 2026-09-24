f39181b5813337744ddcca3e6d564619649cedcc
tibohumblet <tibohumblet@hotmail.com>
2026-09-24 23:50:22 +0200

    Update model
    update:
    - user model, removed following properties: name, password_token,
    email_verief_at>

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,6 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
         'email',
         'password',
     ];
@@ -31,7 +30,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -42,7 +40,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
